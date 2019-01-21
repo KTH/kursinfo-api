50ca4593dd0ddb9e86c54a034a65fea49e9c4fa8
Elena Rakhimova <elenara@kth.se>
2019-01-21 19:39:00 +0100

    Changed controller for selling info

--- server/controllers/SellingInfoCtrl.js
+++ server/controllers/SellingInfoCtrl.js
@@ -1,6 +1,9 @@
 'use strict'
-const { SellingInfo } = require('../models/sellingInfo')
+const { CourseModel } = require('../models/courseModel')
+
 const co = require('co')
+const log = require('kth-node-log')
+
 // const { safeGet } = require('safe-utils')
 // const config = require('../configuration').server
 // const { BasicAPI } = require('kth-node-api-call')
@@ -8,55 +11,51 @@ const co = require('co')
 module.exports = {
   getData: co.wrap(getData),
   postData: co.wrap(postData)
-  // getSellingText: co.wrap(getSellingText)
 }
-
-// let koppsApiInternal = new BasicAPI({
-//   hostname: config.kopps.host,
-//   basePath: '/api/kopps/internal/',
-//   https: false, // config.kopps.https,
-//   json: true,
-//   // Kopps is a public API and needs no API-key
-//   defaultTimeout: config.kopps.defaultTimeout
-// })
 
 function * getData (req, res, next) {
   try {
+    console.log('==Course Code=', req.params.courseCode)
     let doc = {}
     if (process.env.NODE_MOCK) {
       doc = yield { courseCode: 0, sellingText: 'mockSellingText' }
     } else {
-      doc = yield SellingInfo.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
+      doc = yield CourseModel.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
     }
 
     if (!doc) {
       return next()
     }
 
-    res.json({ courseCode: doc.courseCode, sellingText: doc.sellingText })
+    // res.json({ courseCode: doc.courseCode, sellingText_sv: doc.sellingText_sv, sellingText_en: doc.sellingText_en, ImageInfo: doc.ImageInfo })
+    res.json(doc)
   } catch (err) {
+    log.error('Failed to get a sellingText, error:', err)
     next(err)
   }
 }
 
 function * postData (req, res, next) {
   try {
-    let doc = yield SellingInfo.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
-    console.log('=====================cD=========================', req.params.courseCode)
+    console.log('==Update selling text for course', req.params.courseCode, ', language: ', req.body.lang)
+    let doc = yield CourseModel.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
+
+    const textLangStr = `sellingText_${req.body.lang}`
 
     if (!doc) {
-      console.log('=====================SELLING INFO NOT FOUND=========================', req.params.courseCode)
-      doc = new SellingInfo({
+      log.info('Selling info not found for a course: ', req.params.courseCode, 'and will try create a new')
+      doc = new CourseModel({
         courseCode: req.params.courseCode.toUpperCase(),
-        sellingText: req.body.sellingText
+        [textLangStr]: req.body.sellingText
       })
     } else {
-      doc.sellingText = req.body.sellingText
+      doc[textLangStr] = req.body.sellingText
     }
 
     yield doc.save()
-    res.json({ courseCode: doc.courseCode.toUpperCase(), sellingText: doc.sellingText })
+    res.json({ courseCode: doc.courseCode.toUpperCase(), [textLangStr]: req.body.sellingText })
   } catch (err) {
-    next(err)
+    log.error('Failed posting a sellingText, error:', err)
+    next(err) // throw err
   }
 }
