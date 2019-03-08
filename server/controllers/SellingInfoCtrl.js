2ee5babe54bdf3a5b7836703628e84c0666b6577
Elena Rakhimova <elenara@kth.se>
2019-03-08 12:16:34 +0100

    Added post/get of sellingTextAuthor whern user posted a text

--- server/controllers/SellingInfoCtrl.js
+++ server/controllers/SellingInfoCtrl.js
@@ -15,55 +15,58 @@ module.exports = {
 
 function * getData (req, res, next) {
   try {
-    console.log('==Course Code=', req.params.courseCode)
+    const courseCode = req.params.courseCode.toUpperCase()
     let doc = {}
     if (process.env.NODE_MOCK) {
       doc = yield { courseCode: 0, sellingText: 'mockSellingText' }
     } else {
-      doc = yield CourseModel.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
+      doc = yield CourseModel.findOne({ 'courseCode': courseCode })
     }
 
     if (!doc) {
       return next()
     }
-
+    log.info('Get data from kursinfo-api for course', courseCode)
     res.json({ courseCode: doc.courseCode,
       sellingText: {
         en: doc.sellingText_en,
         sv: doc.sellingText_sv
       },
       imageInfo: doc.imageInfo,
-      isCourseWebLink: doc.isCourseWebLink })
+      isCourseWebLink: doc.isCourseWebLink,
+      sellingTextAuthor: doc.sellingTextAuthor })
     // res.json(doc)
   } catch (err) {
-    log.error('Failed to get a sellingText, error:', err)
+    log.error('Failed to get a sellingText, error:', { err })
     next(err)
   }
 }
 
 function * postData (req, res, next) {
   try {
-    console.log('==Update selling text for course', req.params.courseCode)
-    let doc = yield CourseModel.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
-
+    const courseCode = req.params.courseCode.toUpperCase()
+    let doc = yield CourseModel.findOne({ 'courseCode': courseCode })
     const sellingTexts = req.body.sellingText
 
     if (!doc) {
-      log.info('Selling info not found for a course: ', req.params.courseCode, 'and will try create a new')
+      log.info('Selling info not found for a course: ', courseCode, 'and will try create a new')
       doc = new CourseModel({
-        courseCode: req.params.courseCode.toUpperCase(),
+        courseCode: courseCode,
         sellingText_sv: sellingTexts.sv,
-        sellingText_en: sellingTexts.en
+        sellingText_en: sellingTexts.en,
+        sellingTextAuthor: req.body.sellingTextAuthor
       })
     } else {
       doc.sellingText_sv = sellingTexts.sv
       doc.sellingText_en = sellingTexts.en
+      doc.sellingTextAuthor = req.body.sellingTextAuthor
     }
 
     yield doc.save()
+    log.info('==Updated selling text for course', courseCode)
     res.json({ courseCode: doc.courseCode.toUpperCase(), sellingTexts_en: sellingTexts.en, sellingText_sv: sellingTexts.sv })
   } catch (err) {
-    log.error('Failed posting a sellingText, error:', err)
+    log.error('Failed posting a sellingText, error:', { err })
     next(err) // throw err
   }
 }
