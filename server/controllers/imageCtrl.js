a036dbed265d3f16830a1253b186779e0ffb01ea
Elena Rakhimova <elenara@kth.se>
2019-11-14 13:24:42 +0100

    refactored image ctrl

--- server/controllers/imageCtrl.js
+++ server/controllers/imageCtrl.js
@@ -4,39 +4,35 @@ const { CourseModel } = require('../models/courseModel')
 const co = require('co')
 const log = require('kth-node-log')
 
-// const { safeGet } = require('safe-utils')
-// const config = require('../configuration').server
-// const { BasicAPI } = require('kth-node-api-call')
-
 module.exports = {
   getImageInfo: co.wrap(getImageInfo),
   postImageInfo: co.wrap(postImageInfo)
 }
 
-function * getImageInfo (req, res, next) {
+async function getImageInfo (req, res) {
   try {
-    console.log('==Course Code=', req.params.courseCode)
+    log.debug('==Course Code=', req.params.courseCode)
     let doc = {}
     if (process.env.NODE_MOCK) {
-      doc = yield { courseCode: 0, sellingText: 'mockSellingText' }
+      doc = await { courseCode: 0, sellingText: 'mockSellingText' }
     } else {
-      doc = yield CourseModel.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
+      doc = await CourseModel.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
     }
 
     if (!doc) {
-      return next()
+      return
     }
 
     res.json({ courseCode: doc.courseCode, ImageInfo: doc.ImageInfo })
   } catch (err) {
     log.error('Failed to get a sellingText, error:', err)
-    next(err)
+    return err
   }
 }
 
-function * postImageInfo (req, res, next) {
+async function postImageInfo (req, res) {
   try {
-    let doc = yield CourseModel.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
+    let doc = await CourseModel.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
 
     if (!doc) {
       log.info('Course information is not found for a course: ', req.params.courseCode, 'and will try create a new')
@@ -48,10 +44,10 @@ function * postImageInfo (req, res, next) {
       doc.imageInfo = req.body.imageInfo
     }
 
-    yield doc.save()
+    await doc.save()
     res.json({ courseCode: doc.courseCode.toUpperCase(), imageInfo: req.body.imageInfo })
   } catch (err) {
     log.error('Failed posting a ImageInfo, error:', err)
-    next(err) // throw err
+    return err
   }
 }
