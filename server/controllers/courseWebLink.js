2da91f195a73bd3c279377b9aa1a6c6f0bf02ab9
Elena Rakhimova <elenara@kth.se>
2019-11-14 13:24:22 +0100

    Added await/async instead of yield for courseweblink controller

--- server/controllers/courseWebLink.js
+++ server/controllers/courseWebLink.js
@@ -1,23 +1,16 @@
-
-postCourseWebLink
-
 'use strict'
 const { CourseModel } = require('../models/courseModel')
 
 const co = require('co')
 const log = require('kth-node-log')
 
-// const { safeGet } = require('safe-utils')
-// const config = require('../configuration').server
-// const { BasicAPI } = require('kth-node-api-call')
-
 module.exports = {
   postCourseWebLink: co.wrap(postCourseWebLink)
 }
 
-function * postCourseWebLink (req, res, next) {
+async function postCourseWebLink (req, res) {
   try {
-    let doc = yield CourseModel.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
+    let doc = await CourseModel.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
 
     if (!doc) {
       log.info('Course information is not found for a course: ', req.params.courseCode, 'and will try create a new')
@@ -29,10 +22,10 @@ function * postCourseWebLink (req, res, next) {
       doc.isCourseWebLink = req.body.isCourseWebLink
     }
 
-    yield doc.save()
+    await doc.save()
     res.json({ courseCode: doc.courseCode.toUpperCase(), isCourseWebLink: req.body.isCourseWebLink })
   } catch (err) {
     log.error('Failed posting a ImageInfo, error:', err)
-    next(err) // throw err
+    return err // throw err
   }
 }
