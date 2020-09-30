f5f036d45cb77d822645f306770e3f1b7d7d925b
Elena Rakhimova <elenara@kth.se>
2020-09-30 16:59:26 +0200

    refactor: ctrls, models, remove co.wrap

--- server/controllers/courseWebLink.js
+++ server/controllers/courseWebLink.js
@@ -1,31 +1,35 @@
+/* eslint-disable consistent-return */
+
 'use strict'
+
 const { CourseModel } = require('../models/courseModel')
 
-const co = require('co')
 const log = require('kth-node-log')
 
-module.exports = {
-  postCourseWebLink: co.wrap(postCourseWebLink)
-}
-
-async function postCourseWebLink (req, res) {
+async function postCourseWebLink(req, res) {
+  const { courseCode } = req.params
+  const { isCourseWebLink } = req.body
   try {
-    let doc = await CourseModel.findOne({ 'courseCode': req.params.courseCode.toUpperCase() })
+    let doc = await CourseModel.findOne({ courseCode: courseCode.toUpperCase() })
 
     if (!doc) {
-      log.info('Course information is not found for a course: ', req.params.courseCode, 'and will try create a new')
+      log.info('Course information is not found for a course: ', courseCode, 'and will try create a new')
       doc = new CourseModel({
-        courseCode: req.params.courseCode.toUpperCase(),
-        isCourseWebLink: req.body.isCourseWebLink
+        courseCode: courseCode.toUpperCase(),
+        isCourseWebLink,
       })
     } else {
-      doc.isCourseWebLink = req.body.isCourseWebLink
+      doc.isCourseWebLink = isCourseWebLink
     }
 
     await doc.save()
-    res.json({ courseCode: doc.courseCode.toUpperCase(), isCourseWebLink: req.body.isCourseWebLink })
+    res.json({ courseCode: doc.courseCode.toUpperCase(), isCourseWebLink })
   } catch (err) {
     log.error('Failed posting a ImageInfo, error:', err)
     return err // throw err
   }
+}
+
+module.exports = {
+  postCourseWebLink,
 }
