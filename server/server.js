fc265a335ab7697df73571319d5f2ff0c3d1e221
Elena Rakhimova <elenara@kth.se>
2022-09-26 13:03:42 +0200

    fix: remove double authorization for passport

--- server/server.js
+++ server/server.js
@@ -88,8 +88,6 @@ server.use(cookieParser())
  */
 const passport = require('passport')
 require('./authentication')
-server.use(passport.initialize())
-server.use(passport.session())
 
 /* ************************
  * ******* DATABASE *******
